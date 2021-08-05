09faa9dcd7969a164599d47078bcba3f88a8d481
Mandy <84938967+mapalacio19@users.noreply.github.com>
2021-08-05 14:19:42 -0600

    bulma-weather

---
+++ assets/fetch.js
@@ -0,0 +1,17 @@
+class Fetch {
+    async getCurrent(input) {
+      const myKey = "d579661585e4dc8105217f168751a17c";
+  
+      //make request to url
+  
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${d579661585e4dc8105217f168751a17c}`
+      );
+  
+      const data = await response.json();
+  
+      console.log(data);
+  
+      return data;
+    }
+  }
